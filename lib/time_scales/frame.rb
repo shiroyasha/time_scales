8bbbd77773a5eb8fb9ad0cfcde9df8c027d20f2b
Steve Jorgensen <stevej@stevej.name>
2015-04-03 23:28:46 +0000

    Building frame for year-of-scheme

--- lib/time_scales/frame.rb
+++ lib/time_scales/frame.rb
@@ -1,6 +1,19 @@
 module TimeScales
 
   class Frame
+    def self.[](parts)
+      new(parts)
+    end
+
+    def initialize(parts)
+      @year_of_scheme = parts[:year]
+    end
+
+    attr_reader :year_of_scheme
+
+    def year
+      year_of_scheme
+    end
   end
 
 end
--- spec/time_scales/frame_spec.rb
+++ spec/time_scales/frame_spec.rb
@@ -5,13 +5,23 @@ describe TimeScales::Frame do
     subject { described_class[year: 2014] }
 
     it "exposes its year through its #year_of_scheme property" do
-      pending
       expect( subject.year_of_scheme ).to eq( 2014 )
     end
 
     it "exposes its year through its #year property" do
-      pending
       expect( subject.year ).to eq( 2014 )
+    end
+
+    it "is convertible to the time at the start of the year" do
+      pending
+      expect( subject.to_time ).to eq( Time.new(2014, 1, 1, 0, 0, 0) )
+    end
+
+    it "is convertible to range from year start until (but not including) next year start" do
+      pending
+      year_start      = Time.new(2014, 1, 1, 0, 0, 0)
+      next_year_start = Time.new(2015, 1, 1, 0, 0, 0)
+      expect( subject.to_range ).to eq( year_start...next_year_start )
     end
   end
 end
