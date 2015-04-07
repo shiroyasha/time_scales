67f4258f69b67c88253438ed9d6fdae9dd48fb04
Steve Jorgensen <stevej@stevej.name>
2015-04-07 04:18:55 -0700

    Frame part components to dedicated module/file

--- lib/time_scales/frame.rb
+++ lib/time_scales/frame.rb
@@ -2,6 +2,7 @@ require 'time'
 require 'time_scales/frame/assembly_part'
 require 'time_scales/frame/base'
 require 'time_scales/frame/scheme_relative_frame'
+require 'time_scales/frame/part_components'
 
 module TimeScales
 
@@ -37,166 +38,29 @@ module TimeScales
       include Singleton
     end
 
-    module HasYearOfScheme
-      def self.included(other)
-        other.extend HasYearOfScheme::ClassMixin
-      end
-
-      attr_reader :year_of_scheme
-
-      def year
-        year_of_scheme
-      end
-
-      private
-
-      def _initialize(args_array)
-        super
-        @year_of_scheme = ensure_fixnum( args_array.shift )
-      end
-
-      def prepare_time_struct(struct)
-        struct.year = (struct.year || 1) + year_of_scheme - 1
-        super
-      end
-
-      module ClassMixin
-        protected
-
-        def _parts
-          super << Parts::YearOfScheme
-        end
-      end
-    end
-
     class YearOfSchemeOnly < Frame::SchemeRelativeFrame
-      include HasYearOfScheme
+      include PartComponents::HasYearOfScheme
 
       def succ_begin_time
         @end_time ||= Time.new( begin_time.year + 1 )
       end
     end
 
-    module HasMonthOfYear
-      def self.included(other)
-        other.extend HasMonthOfYear::ClassMixin
-      end
-
-      attr_reader :month_of_year
-
-      def month
-        month_of_year
-      end
-
-      private
-
-      def _initialize(args_array)
-        super
-        @month_of_year = ensure_fixnum( args_array.shift )
-      end
-
-      def prepare_time_struct(struct)
-        struct.month = (struct.month || 1) + month_of_year - 1
-        super
-      end
-
-      module ClassMixin
-        protected
-
-        def _parts
-          super << Parts::MonthOfYear
-        end
-      end
-    end
-
     class MonthOfYearOnly < Frame::Base
-      include HasMonthOfYear
-    end
-
-    module HasMonthOfQuarter
-      def self.included(other)
-        other.extend HasMonthOfQuarter::ClassMixin
-      end
-
-      attr_reader :month_of_quarter
-
-      def month
-        month_of_quarter
-      end
-
-      private
-
-      def _initialize(args_array)
-        super
-        @month_of_quarter = ensure_fixnum( args_array.shift )
-      end
-
-      def prepare_time_struct(struct)
-        struct.month = (struct.month || 1) + month_of_quarter - 1
-        super
-      end
-
-      module ClassMixin
-        protected
-
-        def _parts
-          super << Parts::MonthOfQuarter
-        end
-      end
+      include PartComponents::HasMonthOfYear
     end
 
     class MonthOfQuarterOnly < Frame::Base
-      include HasMonthOfQuarter
-    end
-
-    module HasQuarterOfYear
-      def self.included(other)
-        other.extend HasQuarterOfYear::ClassMixin
-      end
-
-      attr_reader :quarter_of_year
-
-      def quarter
-        quarter_of_year
-      end
-
-      private
-
-      def _initialize(args_array)
-        super
-        @quarter_of_year = ensure_fixnum( args_array.shift )
-      end
-
-      def prepare_time_struct(struct)
-        to_month =
-          (struct.month || 1) +
-          3 * (quarter_of_year - 1)
-        if to_month > 12
-          struct.year = (struct.year || 1) + 1
-          to_month -= 12
-          struct.month = to_month
-        else
-          struct.month = to_month
-        end
-        super
-      end
-
-      module ClassMixin
-        protected
-
-        def _parts
-          super << Parts::QuarterOfYear
-        end
-      end
+      include PartComponents::HasMonthOfQuarter
     end
 
     class QuarterOfYearOnly < Frame::Base
-      include HasQuarterOfYear
+      include PartComponents::HasQuarterOfYear
     end
 
     class QuarterOfYear_Month < Frame::Base
-      include HasQuarterOfYear
-      include HasMonthOfQuarter
+      include PartComponents::HasQuarterOfYear
+      include PartComponents::HasMonthOfQuarter
     end
 
     module HasMonthOfSchemePrecision
@@ -215,8 +79,8 @@ module TimeScales
     end
 
     class YearOfScheme_Month < SchemeRelativeFrame
-      include HasYearOfScheme
-      include HasMonthOfYear
+      include PartComponents::HasYearOfScheme
+      include PartComponents::HasMonthOfYear
       include HasMonthOfSchemePrecision
     end
 
@@ -236,15 +100,15 @@ module TimeScales
     end
 
     class YearOfScheme_Quarter < SchemeRelativeFrame
-      include HasYearOfScheme
-      include HasQuarterOfYear
+      include PartComponents::HasYearOfScheme
+      include PartComponents::HasQuarterOfYear
       include HasQuarterOfSchemePrecision
     end
 
     class YearOfScheme_Quarter_Month < SchemeRelativeFrame
-      include HasYearOfScheme
-      include HasQuarterOfYear
-      include HasMonthOfQuarter
+      include PartComponents::HasYearOfScheme
+      include PartComponents::HasQuarterOfYear
+      include PartComponents::HasMonthOfQuarter
       include HasMonthOfSchemePrecision
     end
 
---
+++ lib/time_scales/frame/part_components.rb
@@ -0,0 +1,146 @@
+module TimeScales
+  module Frame
+
+    module PartComponents
+
+      module HasYearOfScheme
+        def self.included(other)
+          other.extend HasYearOfScheme::ClassMixin
+        end
+
+        attr_reader :year_of_scheme
+
+        def year
+          year_of_scheme
+        end
+
+        private
+
+        def _initialize(args_array)
+          super
+          @year_of_scheme = ensure_fixnum( args_array.shift )
+        end
+
+        def prepare_time_struct(struct)
+          struct.year = (struct.year || 1) + year_of_scheme - 1
+          super
+        end
+
+        module ClassMixin
+          protected
+
+          def _parts
+            super << Parts::YearOfScheme
+          end
+        end
+      end
+
+      module HasMonthOfYear
+        def self.included(other)
+          other.extend HasMonthOfYear::ClassMixin
+        end
+
+        attr_reader :month_of_year
+
+        def month
+          month_of_year
+        end
+
+        private
+
+        def _initialize(args_array)
+          super
+          @month_of_year = ensure_fixnum( args_array.shift )
+        end
+
+        def prepare_time_struct(struct)
+          struct.month = (struct.month || 1) + month_of_year - 1
+          super
+        end
+
+        module ClassMixin
+          protected
+
+          def _parts
+            super << Parts::MonthOfYear
+          end
+        end
+      end
+
+      module HasMonthOfQuarter
+        def self.included(other)
+          other.extend HasMonthOfQuarter::ClassMixin
+        end
+
+        attr_reader :month_of_quarter
+
+        def month
+          month_of_quarter
+        end
+
+        private
+
+        def _initialize(args_array)
+          super
+          @month_of_quarter = ensure_fixnum( args_array.shift )
+        end
+
+        def prepare_time_struct(struct)
+          struct.month = (struct.month || 1) + month_of_quarter - 1
+          super
+        end
+
+        module ClassMixin
+          protected
+
+          def _parts
+            super << Parts::MonthOfQuarter
+          end
+        end
+      end
+
+      module HasQuarterOfYear
+        def self.included(other)
+          other.extend HasQuarterOfYear::ClassMixin
+        end
+
+        attr_reader :quarter_of_year
+
+        def quarter
+          quarter_of_year
+        end
+
+        private
+
+        def _initialize(args_array)
+          super
+          @quarter_of_year = ensure_fixnum( args_array.shift )
+        end
+
+        def prepare_time_struct(struct)
+          to_month =
+            (struct.month || 1) +
+            3 * (quarter_of_year - 1)
+          if to_month > 12
+            struct.year = (struct.year || 1) + 1
+            to_month -= 12
+            struct.month = to_month
+          else
+            struct.month = to_month
+          end
+          super
+        end
+
+        module ClassMixin
+          protected
+
+          def _parts
+            super << Parts::QuarterOfYear
+          end
+        end
+      end
+
+    end
+
+  end
+end
