d72f2b017ca2ed563d049e5de48848257cf722dd
Steve Jorgensen <stevej@stevej.name>
2015-04-07 10:24:57 -0700

    Factor out dup from quarter & month precisions

--- lib/time_scales/frame/precisions.rb
+++ lib/time_scales/frame/precisions.rb
@@ -9,29 +9,44 @@ module TimeScales
         end
       end
 
-      module HasMonthOfSchemePrecision
+      module Has_N_MonthsOfSchemePrecision
         def succ_begin_time
           @end_time ||= begin
             succ_y = year_of_scheme
-            succ_m = begin_time.month + 1
+            succ_m = begin_time.month + n_months_precision
             if succ_m > 12
               succ_y += 1 ; succ_m = 1
             end
             Time.new( succ_y, succ_m )
           end
         end
+
+        private
+
+        # Precision unit size in months. A year must be divisible
+        # by this value.
+        def n_months_precision
+          raise NotImplementedError, "Subclass responsibility"
+        end
+      end
+
+      module HasMonthOfSchemePrecision
+        include Has_N_MonthsOfSchemePrecision
+
+        private
+
+        def n_months_precision
+          1
+        end
       end
 
       module HasQuarterOfSchemePrecision
-        def succ_begin_time
-          @end_time ||= begin
-            succ_y = year_of_scheme
-            succ_m = begin_time.month + 3
-            if succ_m > 12
-              succ_y += 1 ; succ_m = 1
-            end
-            Time.new( succ_y, succ_m )
-          end
+        include Has_N_MonthsOfSchemePrecision
+
+        private
+
+        def n_months_precision
+          3
         end
       end
 
